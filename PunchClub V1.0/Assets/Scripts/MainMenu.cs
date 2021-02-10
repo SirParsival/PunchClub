4501cf54dd808b1b594a6533a191f3850c7c7fa0
SirParsival <60931696+SirParsival@users.noreply.github.com>
2021-02-10 18:08:02 +0200

    Main Menu

---
+++ PunchClub V1.0/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    public void GoToGame()
+    {
+        //3
+        SceneManager.LoadScene("Game");
+    }
+}
