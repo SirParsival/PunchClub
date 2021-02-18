1af8e070b662a1b99c6160d74bab2b653449d2ca
SirParsival <60931696+SirParsival@users.noreply.github.com>
2021-02-18 20:03:02 +0200

    Moving the hero around, part 2

--- PunchClub V1.0/Assets/Scripts/Hero.cs
+++ PunchClub V1.0/Assets/Scripts/Hero.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    //1
+    public Animator baseAnim;
+    public Rigidbody body;
+    //public SpriteRenderer shadowSprite;
+    //2
+    public float speed = 2;
+    public float walkSpeed = 2;
+    //3
+    Vector3 currentDir;
+    bool isFacingLeft;
+    protected Vector3 frontVector;
+
     void Update()
     {
-        
+        //1
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        //2
+        currentDir = new Vector3(h, 0, v);
+        currentDir.Normalize();
+        //3
+        if ((v == 0 && h == 0))
+        {
+            Stop();
+        }
+        else if ((v != 0 || h != 0))
+        {
+            Walk();
+        }
+    }
+
+    //1
+    public void Stop()
+    {
+        speed = 0;
+        baseAnim.SetFloat("Speed", speed);
+    }
+    //2
+    public void Walk()
+    {
+        speed = walkSpeed;
+        baseAnim.SetFloat("Speed", speed);
+    }
+
+    //1
+    void FixedUpdate()
+    {
+        Vector3 moveVector = currentDir * speed;
+        body.MovePosition(transform.position + moveVector *
+       Time.fixedDeltaTime);
+        baseAnim.SetFloat("Speed", moveVector.magnitude);
+        //2
+        if (moveVector != Vector3.zero)
+        {
+            if (moveVector.x != 0)
+            {
+                isFacingLeft = moveVector.x < 0;
+            }
+            FlipSprite(isFacingLeft);
+        }
+    }
+    //3
+    public void FlipSprite(bool isFacingLeft)
+    {
+        if (isFacingLeft)
+        {
+            frontVector = new Vector3(-1, 0, 0);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            frontVector = new Vector3(1, 0, 0);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
