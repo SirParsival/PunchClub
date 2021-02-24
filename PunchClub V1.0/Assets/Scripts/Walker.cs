bb2e8f3dd298c66eb9f76baf64805fc140c077af
SirParsival <60931696+SirParsival@users.noreply.github.com>
2021-02-24 17:37:33 +0200

    Making the robots walk

--- PunchClub V1.0/Assets/Scripts/Walker.cs
+++ PunchClub V1.0/Assets/Scripts/Walker.cs
@@ -1,23 +1,23 @@
-﻿//using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
-
-
+//1
 [RequireComponent(typeof(Actor))]
 public class Walker : MonoBehaviour
 {
+    //2
     public NavMeshAgent navMeshAgent;
     private NavMeshPath navPath;
     private List<Vector3> corners;
-
+    //3
     float currentSpeed;
     float speed;
-
+    //4
     private Actor actor;
     private System.Action didFinishWalk;
 
+    //5
     void Start()
     {
         navMeshAgent.updatePosition = false;
@@ -54,7 +54,8 @@ public class Walker : MonoBehaviour
         if (canWalk && corners != null && corners.Count > 0)
         {
             currentSpeed = speed;
-            actor.body.MovePosition(Vector3.MoveTowards(transform.position, corners[0], Time.fixedDeltaTime * speed));
+            actor.body.MovePosition(Vector3.MoveTowards(transform.position, corners[0],
+            Time.fixedDeltaTime * speed));
             //3
             if (Vector3.SqrMagnitude(
             transform.position - corners[0]) < 0.6f)
@@ -81,3 +82,4 @@ public class Walker : MonoBehaviour
         }
         actor.baseAnim.SetFloat("Speed", currentSpeed);
     }
+}
