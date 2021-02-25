83eebc62260b243e5b03c8d3a47fad8bdec6c578
SirParsival <60931696+SirParsival@users.noreply.github.com>
2021-02-25 13:35:38 +0200

    Assemble all the robots!

--- PunchClub V1.0/Assets/Scripts/Actor.cs
+++ PunchClub V1.0/Assets/Scripts/Actor.cs
@@ -92,7 +92,7 @@ public class Actor : MonoBehaviour
     public virtual void DidHitObject(Collider collider, Vector3 hitPoint, Vector3 hitVector)
     {
         Actor actor = collider.GetComponent<Actor>();
-        if (actor != null && actor.CanBeHit())
+        if (actor != null && actor.CanBeHit() && collider.tag != gameObject.tag)
         {
             if (collider.attachedRigidbody != null)
             {
