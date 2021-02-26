6ca977b4d35e4c34d40043ede92dd1f3ea964cf8
SirParsival <60931696+SirParsival@users.noreply.github.com>
2021-02-26 18:54:54 +0200

    data

---
+++ PunchClub V1.0/Assets/Scripts/LevelData.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName ="LevelData", menuName = "PunchClub/LevelData")]
+public class LevelData : ScriptableObject 
+{
+	public List<BattleEvent> battleData;
+	public GameObject levelPrefab;
+	public string levelName;
+}
+
+[Serializable]
+public class BattleEvent 
+{
+	public int column;
+	public List<EnemyData> enemies;
+}
+
+[Serializable]
+public class EnemyData 
+{
+	public EnemyType type;
+	public RobotColor color;
+	public int row;
+	public float offset;
+}
+
+public enum EnemyType 
+{
+	Robot = 0,
+	Boss
+}
