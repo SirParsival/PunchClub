fdb333ae8dfa124f6ea34b52328aa2d3867c49cc
SirParsival <60931696+SirParsival@users.noreply.github.com>
2021-03-05 16:21:17 +0200

    on-the-go with mobile UI

--- PunchClub V1.0/Assets/Scripts/InputHandler.cs
+++ PunchClub V1.0/Assets/Scripts/InputHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputHandler : MonoBehaviour
 {
@@ -8,51 +9,123 @@ public class InputHandler : MonoBehaviour
     float horizontal;
     float vertical;
     bool jump;
+
     float lastJumpTime;
     bool isJumping;
     public float maxJumpDuration = 0.2f;
+
+    bool didAttack;
+    public bool useUI = true;
     //1
     public float GetVerticalAxis()
     {
         return vertical;
     }
+
     public float GetHorizontalAxis()
     {
         return horizontal;
-    }
-    public bool GetJumpButtonDown()
-    {
-        return jump;
-    }
-
-    void Update()
-    {
-        //2
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        attack = Input.GetButtonDown("Attack");
-        //3
-        if (!jump && !isJumping && Input.GetButton("Jump"))
-        {
-            jump = true;
-            lastJumpTime = Time.time;
-            isJumping = true;
-        }
-        else if (!Input.GetButton("Jump"))
-        {
-            //4
-            jump = false;
-            isJumping = false;
-        }
-        //5
-        if (jump && Time.time > lastJumpTime + maxJumpDuration)
-        {
-            jump = false;
-        }
     }
 
     public bool GetAttackButtonDown()
     {
         return attack;
+    }
+
+    public bool GetJumpButtonDown()
+    {
+        return jump;
+    }
+
+    public void DidPressAttack(BaseEventData data)
+    {
+        attack = true;
+        didAttack = false;
+    }
+
+    public void DidPressJump(BaseEventData data)
+    {
+        if (!jump)
+        {
+            jump = true;
+            lastJumpTime = Time.time;
+        }
+    }
+
+    public void DidReleaseJump(BaseEventData data)
+    {
+        jump = false;
+    }
+
+    public Vector2 VectorForPadDirection(ActionDPad.ActionPadDirection padDirection)
+    {
+        float maxX = 1.0f;
+        float maxY = 1.1f;
+        switch (padDirection)
+        {
+            case ActionDPad.ActionPadDirection.None:
+                return Vector2.zero;
+            case ActionDPad.ActionPadDirection.Up:
+                return new Vector2(0, maxY);
+            case ActionDPad.ActionPadDirection.UpRight:
+                return new Vector2(maxX, maxY);
+            case ActionDPad.ActionPadDirection.Right:
+                return new Vector2(maxX, 0);
+            case ActionDPad.ActionPadDirection.DownRight:
+                return new Vector2(maxX, -maxY);
+            case ActionDPad.ActionPadDirection.Down:
+                return new Vector2(0, -maxY);
+            case ActionDPad.ActionPadDirection.DownLeft:
+                return new Vector2(-maxX, -maxY);
+            case ActionDPad.ActionPadDirection.Left:
+                return new Vector2(-maxX, 0);
+            case ActionDPad.ActionPadDirection.UpLeft:
+                return new Vector2(-maxX, maxY);
+            default:
+                return Vector2.zero;
+        }
+    }
+
+    public void OnActionPadChangeDirection(ActionDPad.ActionPadDirection direction)
+    {
+        Vector2 directionVector = VectorForPadDirection(direction);
+        horizontal = directionVector.x;
+        vertical = directionVector.y;
+    }
+
+    void Update()
+    {
+        if (useUI)
+        {
+            if (didAttack)
+            {
+                didAttack = attack = false;
+            }
+            else if (attack)
+            {
+                didAttack = true;
+            }
+        }
+        else
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+            attack = Input.GetButtonDown("Attack");
+            if (!jump && !isJumping && Input.GetButton("Jump"))
+            {
+                jump = true;
+                lastJumpTime = Time.time;
+                isJumping = true;
+            }
+            else if (!Input.GetButton("Jump"))
+            {
+                jump = false;
+                isJumping = false;
+            }
+        }
+        if (jump && Time.time > lastJumpTime + maxJumpDuration)
+        {
+            jump = false;
+        }
     }
 }
